ac92531c81a582153719259ecf45b6aba4c813b2
fffffifty <fifty9426464@gmail.com>
2020-10-22 14:54:17 +0800

    dependencies修改

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export const Greeter = (name: string) => `Hello ${name}`;
 export function add(a: number, b: number): number {
   return a + b;
 }
+export function sum(a: number, b: number): number {
+  return a + b;
+}
